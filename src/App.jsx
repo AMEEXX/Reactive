4a8d3d2eb28147b429e0692913bbc9f9909d6175
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-07-29 08:02:44 +0000

    useEffect and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-  
-const Card = ({title}) => {
-  const [hasLiked, sethasliked] = useState(false)
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const Card = ({ title }) => {
+  const [hasLiked, sethasliked] = useState(true);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${title} has been liked : ${hasLiked}`);
+  }, [hasLiked]);
+
+
+
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div
+      className="card"
+      onClick={() => setCount(count + 1)}
+      style={{ color: "blue" }}
+    >
+      <h2>
+        {title} <br /> {count}
+      </h2>
+
       <button onClick={() => sethasliked(!hasLiked)}>
         {hasLiked ? "Like" : "Liked"}
       </button>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-  const App = () => {
-    return (
-      <div className='card-container'>
-        {/* <h2>Functional Component</h2> */}
-        <Card title = "3 Idiots"/>
-        <Card title = "Lion King"/>
-        <Card title = "Avatar"/>
-       
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="card-container">
+      {/* <h2>Functional Component</h2> */}
+      <Card title="3 Idiots" />
+      <Card title="Lion King" />
+      <Card title="Avatar" />
+    </div>
+  );
+};
 
-
-export default App
-    
+export default App;
