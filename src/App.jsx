10609640e592fa8d0e6caee3aca1b3f23a3c9b68
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-07-30 21:39:11 +0000

    "new changes"

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,27 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-const Card = ({ title }) => {
-  const [hasLiked, sethasliked] = useState(true);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`${title} has been liked : ${hasLiked}`);
-  }, [hasLiked]);
-
-
-
-  return (
-    <div
-      className="card"
-      onClick={() => setCount(count + 1)}
-      style={{ color: "blue" }}
-    >
-      <h2>
-        {title} <br /> {count}
-      </h2>
-
-      <button onClick={() => sethasliked(!hasLiked)}>
-        {hasLiked ? "Like" : "Liked"}
-      </button>
-    </div>
-  );
-};
+import Search from "./components/Search.jsx";
+import search from "./components/Search.jsx";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="card-container">
-      {/* <h2>Functional Component</h2> */}
-      <Card title="3 Idiots" />
-      <Card title="Lion King" />
-      <Card title="Avatar" />
-    </div>
+    <main>
+      <div className="pattern" />
+          
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> That You Like
+            Without Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+       
+      </div>
+    </main>
   );
 };
 
